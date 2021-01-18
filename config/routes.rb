3b212f384790fa9d1bb05faf5d6d33d08611ec98
ProCode2 <bpro249@gmail.com>
2021-01-18 17:53:26 +0530

    change users/login route to post

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :todos
-  get "users/login", to: "users#login"
+  post "users/login", to: "users#login"
   resources :users
 end
